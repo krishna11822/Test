37c0e72e37a4e52d0e71a80fa88bc2da7cdb456b
Veera Tetala <veera.tetala@infor.com>
2022-07-20 09:49:54 +0530

    version 2 hello

--- helloworld.vb
+++ helloworld.vb
@@ -1,6 +1,6 @@
 Imports System
 Module Module1
-   'This program will display Hello World krishna'
+   'This program will display Hello World krishna version 2'
    Sub Main()
       Console.WriteLine("Hello World")
       Console.ReadKey()
